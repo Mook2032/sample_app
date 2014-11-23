8cc6cc67ce3afa901971b19b78a88aaa18edb23e
Mook2032 <curseofwallachia@gmail.com>
2014-11-23 20:29:49 +0700

    finished week7?

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,10 @@ class UsersControllerTest < ActionController::TestCase
   test "should get register" do
     get :register
     assert_response :success
+
+  test "should get login" do
+    get :login
+    assert_response :success
   end
 
 end
